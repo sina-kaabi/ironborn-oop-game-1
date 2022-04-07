472ac276c42826f6e72ca13f1881409b0f47f5eb
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-04-07 17:39:29 +0200

    create and move multiple obstacles (same position)

--- js/game.js
+++ js/game.js
@@ -2,34 +2,41 @@
 
 class Game {
     constructor(create, draw){
+        this.time = 0;
         this.player = null;
+        this.obstacles = [];
         this.create = create;
         this.draw = draw;
     }
 
     start(){
+        
         // create & draw player
         this.player = new Player();
         this.player.domElement = this.create("player"); //create a dom element with the class "player"
         this.draw(this.player);
 
-        // create & draw an obstacle
-        this.obstacle = new Obstacle();
-        this.obstacle.domElement = this.create("obstacle");
-        this.draw(this.obstacle);
-
-
-        
-
         setInterval( () => {
-            // move obstacle
-            this.obstacle.moveDown();
-            this.draw(this.obstacle);
-        }, 50);
+            
+            // move obstacles
+            this.obstacles.forEach( (obstacle) => {
+                obstacle.moveDown();
+                this.draw(obstacle);
+            });
 
-        
+            // create & draw an obstacles
+            if(this.time % 30 === 0){
+                const newObstacle = new Obstacle();
+                newObstacle.domElement = this.create("obstacle");
+                this.obstacles.push(newObstacle);
+            }
+
+            this.time++;
+
+        }, 50);        
     }
 
+    
     movePlayer(direction){
         if(direction === "left"){
             this.player.moveLeft();
