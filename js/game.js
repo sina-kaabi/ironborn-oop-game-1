3908e53871edca92f0355e5a8a6046abd4a2f2d9
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-04-07 15:26:55 +0200

    functionality to draw and move player

--- js/game.js
+++ js/game.js
@@ -1,1 +1,43 @@
-console.log("hello world")
+
+
+class Game {
+    constructor(create, draw){
+        this.player = null;
+        this.create = create;
+        this.draw = draw;
+    }
+
+    start(){
+        this.player = new Player();
+        this.player.domElement = this.create("player"); //create a dom element with the class "player"
+        this.draw(this.player);
+    }
+
+    movePlayer(direction){
+        if(direction === "left"){
+            this.player.moveLeft();
+        } else if (direction === "right"){
+            this.player.moveRight();
+        }
+        this.draw(this.player);
+    }
+}
+
+
+class Player {
+    constructor() {
+        this.positionX = 50;
+        this.positionY = 0;
+        this.domElement = null;
+    }
+
+    moveLeft() {
+        this.positionX--;
+    }
+
+    moveRight() {
+        this.positionX++;
+    }
+}
+
+
